364e399aa830bea799f5fa43b9f0e22f08c779e6
SmartCoderI <yuqingwula@gmail.com>
2025-04-18 19:24:11 -0700

    final IW

---
+++ src/edu/upenn/cit594/datamanagement/CSVFormatException.java
@@ -0,0 +1,9 @@
+package edu.upenn.cit594.datamanagement;
+
+
+public class CSVFormatException extends Exception {
+    public CSVFormatException(String message) {
+        super(message);
+    }
+}
+
